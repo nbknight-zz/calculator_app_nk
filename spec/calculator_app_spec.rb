358c97fff39843faf7ca93b52de91d2c57f67d3f
nbknight <nicholas.b.knight@gmail.com>
2018-08-14 22:29:21 -0400

    Add tests and methods. 4 passing tests. Needs refractoring.

--- spec/calculator_app_spec.rb
+++ spec/calculator_app_spec.rb
@@ -1,1 +1,35 @@
 require_relative '../calculator_app'
+
+describe "Calculator" do
+	it 'Takes two arguments and returns the sum' do
+		calculator = Calculator.new
+		sum = calculator.add(5,5)
+
+		expect(sum).to eq(10)
+	end
+
+	it 'Takes two arguments and returns the product' do
+		calculator = Calculator.new
+		product = calculator.multiply(5,5)
+
+		expect(product).to eq(25)
+	end
+
+
+
+	it 'Takes two arguments and returns the difference' do
+		calculator = Calculator.new
+		difference = calculator.subtract(5,5)
+
+		expect(difference).to eq(0)
+	end
+
+
+
+	it 'Takes two arguments and returns the quotient' do
+		calculator = Calculator.new
+		quotient = calculator.divide(5,5)
+
+		expect(quotient).to eq(1)
+	end
+end
